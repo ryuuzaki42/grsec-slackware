75f6fc1f491258ed3b96bbba7ee61a51d42fe89d
João Batista <joao42lbatista@gmail.com>
2016-10-31 22:08:22 -0200

    Adding others

---
+++ others/good_links.r
@@ -0,0 +1,9 @@
+https://wiki.archlinux.org/index.php/Kernels/Traditional_compilation
+
+https://en.wikibooks.org/wiki/Grsecurity/Appendix/Grsecurity_and_PaX_Configuration_Options
+
+https://github.com/pyllyukko/grsec_confnbuild
+
+https://github.com/drgibbon/grsec-slackware
+
+https://github.com/ryuuzaki42/grsec-slackware
